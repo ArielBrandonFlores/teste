18b059aef93f6d129276a820a8cc37c42c0205d8
ArielBrandonFlores <brandon.giron.flores@gmail.com>
2026-06-17 17:04:22 -0300

    add strncmp header

--- libft/libft.h
+++ libft/libft.h
@@ -27,6 +27,7 @@ void	*ft_memset(void *src, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *str, int c);
 char	*strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 
 #endif

afcc0bd96ece86319903e14d8228a1c1b3791c7f
ArielBrandonFlores <brandon.giron.flores@gmail.com>
2026-06-19 16:45:25 -0300

    create calloc

---
+++ libft/ft_calloc.c
@@ -0,0 +1,16 @@
+#include"libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	if (nmemb != 0 && size > ((size_t)-1) / nmemb)
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, total_size);
+	return (ptr);
+}

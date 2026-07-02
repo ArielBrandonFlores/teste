7dfc4d28833b5cd8b5e45d2ae1dcefc3d869b69b
Ariel Brandon Giron Flores <ariel-flores@42Ubuntu.localdomain>
2026-07-01 22:40:55 -0300

    fd functions created

--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,7 @@ int	ft_atoi(const char *nptr);
 int	ft_strlen(const char *array);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
 char	*ft_itoa(int n);
 char	*ft_strdup(const char *s);
 char	**ft_split(char const *s, char c);
@@ -40,11 +41,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 void	ft_bzero(void *src, size_t n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memset(void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 size_t	ft_count_words(char const *s, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
